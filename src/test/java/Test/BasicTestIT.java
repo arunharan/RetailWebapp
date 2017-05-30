478f3d12b5d8342a449a62762292cb88975dc654
arunharan <am88218@gmail.com>
2017-05-30 10:33:42 +0000

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -16,7 +16,7 @@ public class BasicTestIT {
 	 // WebDriver	driver = new ChromeDriver();
 	 //WebDriver	driver = new FirefoxDriver();
 	 WebDriver	driver = new HtmlUnitDriver();
-	  driver.get("http://172.16.2.78:8080/retailone/");
+	  driver.get("http://172.16.2.78:8080/retailone-3.0/");
 	  //driver.get("https://devopsdemoapp.cfapps.io/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
 	  driver.close();
